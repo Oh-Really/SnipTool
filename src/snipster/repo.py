a48f39ce0c1584563b33806f429bd323a72c104d
Oh-Really <moreilly406@gmail.com>
2025-10-20 21:57:38 +0100

    Finished defining the database repo class

--- src/snipster/repo.py
+++ src/snipster/repo.py
@@ -1,8 +1,10 @@
 from abc import ABC, abstractmethod
 from typing import Sequence
 
+from sqlmodel import Session, select
+
 from .exceptions import SnippetNotFoundError
-from .models import Snippet
+from .models import Snippet, engine
 
 
 class SnippetRepository(ABC):
@@ -34,7 +36,7 @@ class InMemorySnippetRepository(SnippetRepository):
     def add(self, snippet: Snippet) -> None:
         next_id = max(self._data.keys(), default=0) + 1
         self._data[next_id] = snippet
-        return self._data
+        # return self._data
 
     def get(self, snippet_id: int) -> Snippet | None:
         return self._data.get(snippet_id)
@@ -54,4 +56,53 @@ class InMemorySnippetRepository(SnippetRepository):
 
 
 class DatabaseSnippetRepository(SnippetRepository):
-    pass
+    def __init__(self, engine):
+        self.engine = engine
+
+    def add(self, snippet: Snippet) -> None:
+        with Session(engine) as session:
+            session.add(snippet)
+            session.commit()
+            session.refresh(snippet)
+        # return snippet
+
+    def get(self, snippet_id: int) -> Snippet | None:
+        with Session(self.engine) as session:
+            statement = select(Snippet).where(Snippet.id == snippet_id)
+            result = session.exec(statement).first()
+            return result
+
+    def list(self) -> Snippet:
+        with Session(self.engine) as session:
+            statement = select(Snippet)
+            result = session.exec(statement).all()
+            return result
+
+    def delete(self, snippet_id: int) -> None:
+        with Session(self.engine) as session:
+            statement = select(Snippet).where(Snippet.id == snippet_id)
+            snip_to_delete = session.exec(statement).first()
+            if not snip_to_delete:
+                raise SnippetNotFoundError(f"Snippet with id {snippet_id} not found")
+
+            session.delete(snip_to_delete)
+            session.commit()
+
+    def update(self, snippet_id: int, updated_snippet: Snippet) -> None:
+        with Session(self.engine) as session:
+            statement = select(Snippet).where(Snippet.id == snippet_id)
+            snippet = session.exec(statement).first()
+
+            if snippet is None:
+                raise SnippetNotFoundError(f"Snippet with id {snippet_id} not found")
+
+            snippet.title = updated_snippet.title
+            snippet.code = updated_snippet.code
+            snippet.description = updated_snippet.description
+            snippet.favourite = updated_snippet.favourite
+
+            session.add(snippet)
+            session.commit()
+            session.refresh(snippet)
+
+            return snippet
